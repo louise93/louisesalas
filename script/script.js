c040caaccca55e3d749e7a298fe1b694d05e6d57
Louise <louisesalas8.26@gmail.com>
2018-09-02 13:24:17 +0800

    Updated script.js

--- script/script.js
+++ script/script.js
@@ -25,7 +25,10 @@
 
   					  	{"commands": { "command" : "skills" , "desc" : '<p>List of Skills<p><ul><li>PHP Laravel</li><li>PHP Codeigniter</li><li>MySQL</li><li>Vue.js</li><li>Node.js</li><li>Pythong (Currently Studying)</li></ul>' }},
   					  	{"commands" :  { "command" : "education" , "desc" : "<p>Education</p><p>ACLC College of Butuan  - <small>(BSIT ,(2012-2016))</small></p>"}},
-  					  	{"commands" : { "command" : "experience" , "desc" : "<p>Experience</p><p>Lead Programmer  - <small>(Interday Manrkets Management ,(2017-present))</small></p><p>Programmer  - <small>(FVL Travel and Tours ,(May 2017-August 2017))</small></p><p>Junior Web Developer (OJT)  - <small>(EngTech Global Solutions ,(Dec. 2015-Mar. 2016))</small></p>"}}
+  					  	{"commands" : { "command" : "experience" , "desc" : "<p>Experience</p><p>Lead Programmer  - <small>(Interday Manrkets Management ,(2017-present))</small></p><p>Programmer  - <small>(FVL Travel and Tours ,(May 2017-August 2017))</small></p><p>Junior Web Developer (OJT)  - <small>(EngTech Global Solutions ,(Dec. 2015-Mar. 2016))</small></p>"}},
+  					  	{"commands"  : {"command" : "social_media" , "desc" : "To be updated soon"}},
+  					  	{"commands" : {"command" : "projects" ,"desc" : "To be updated soon."}},
+  					  	{"commands" : {"command" : "about" ,"desc" : "<p>Hello there ! . I am Louise Salas . I'm just another Web Developer. I  don't fixed defective phones,laptops and computers. I simply write code. I do backend works. Sometimes I'm a developer. Sometimes a server. (I'm  not into server management but was forced to do so hahah . I don't know much about server management really. Good thing, Google exist heheh) </p>"}}
 
 
 					  ];
